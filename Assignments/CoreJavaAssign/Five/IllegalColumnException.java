9e9aef3d2f0daf67997c7aeb87fc2415deb19976
Balaji25062001 <balajisonofprema@gmail.com>
2024-04-01 11:46:10 +0530

    My Assign

---
+++ Assignments/CoreJavaAssign/Five/IllegalColumnException.java
@@ -0,0 +1,10 @@
+package classnine;
+
+public class IllegalColumnException extends Exception {
+	private static final long serialVersionUID = 1L;
+
+	public IllegalColumnException(String exception) {
+		super(exception);
+	}
+
+}
